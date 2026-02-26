54ffee36cc377b6d61e22b532633c5d881878aa8
charrasanaelcharras-maker <charrasanaelcharras@gmail.com>
2026-02-25 22:17:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const lightbox = document.getElementById("lightbox");
-const lightboxImg = document.getElementById("lightbox-img");
-const closeBtn = document.querySelector(".close");
+document.addEventListener("DOMContentLoaded", function() {
 
-document.querySelectorAll(".gallery img").forEach(img => {
-  img.addEventListener("click", () => {
-    lightbox.style.display = "block";
-    lightboxImg.src = img.src;
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+  const closeBtn = document.querySelector(".close");
+
+  // ===== LIGHTBOX =====
+  document.querySelectorAll(".gallery img").forEach(img => {
+    img.addEventListener("click", () => {
+      lightbox.style.display = "block";
+      lightboxImg.src = img.src;
+    });
   });
+
+  closeBtn.addEventListener("click", () => {
+    lightbox.style.display = "none";
+  });
+
+  // ===== POPUP =====
+  const popup = document.getElementById("popup-aviso");
+  const cerrar = document.getElementById("cerrar-popup");
+
+  setTimeout(() => {
+    cerrar.style.display = "flex";
+  }, 1000);
+
+  cerrar.addEventListener("click", function() {
+    popup.style.display = "none";
+  });
+
 });
 
-closeBtn.addEventListener("click", () => {
-  lightbox.style.display = "none";
-});
